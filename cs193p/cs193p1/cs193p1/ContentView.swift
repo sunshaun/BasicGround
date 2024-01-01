a30144f1763a1c18f8002a0bbc5d08d5c64d4302
sunshaun <bangysn@gmail.com>
2024-01-01 23:24:11 +0900

    Day1: view practice

---
+++ cs193p/cs193p1/cs193p1/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  cs193p1
+//
+//  Created by Fong on 1/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var emojis = ["🦼", "🚡", "🚨", "🚛", "🚗", "🚝", "🚞", "🚋", "🚃", "🚖", "🚑", "🚟", "🎾", "🏒", "🎱", "🏓", "🏉", "⚾️", "🏈", "🏀"]
+    // @State makes variable as a pointer
+    @State var emojicount = 14
+    var body: some View {
+        VStack{
+            ScrollView{
+                LazyVGrid(columns:[GridItem(.adaptive(minimum: 87))]){
+                    // id parameter: for identify
+                    ForEach(emojis[0..<emojicount], id: \.self) {
+                        emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }.foregroundColor(.red)
+            }
+        }
+        HStack {
+            add
+            Spacer()
+            remove
+        }.font(.largeTitle).padding(.horizontal)
+        
+    }
+
+    
+    var add: some View {
+        Button(action: {
+            if emojicount < emojis.count{
+                emojicount += 1
+            }
+        }, label: {
+            // this call need SF Symbols application
+            Image(systemName: "plus.diamond")
+        })
+    }
+    
+    var remove: some View {
+        Button(action: {
+            if emojicount > 1{
+                emojicount -= 1
+            }
+        }, label: {
+            Image(systemName: "minus.diamond")
+        })
+    }
+}
+
+
+struct CardView: View{
+    @State var isFaceup = true
+    var content: String
+    var body: some View {
+        let shape = RoundedRectangle(cornerRadius: 20)
+        
+        ZStack{
+            if isFaceup {
+                shape.fill(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            }
+            else {
+                shape.fill()
+            }
+        }.onTapGesture{
+            isFaceup = !isFaceup
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+//    dark mode
+//    .preferredColorScheme(.dark)
+
+}
---
+++ cs193p/cs193p1/cs193p1/cs193p1App.swift
@@ -0,0 +1,17 @@
+//
+//  cs193p1App.swift
+//  cs193p1
+//
+//  Created by Fong on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct cs193p1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
